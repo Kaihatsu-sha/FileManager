ffc257da16f65d21539b0f780bda04d82dc56c2e
Kaihatsu <unknown.kaihatsu-sha@yandex.ru>
2022-01-05 18:02:55 +0500

    Очистка проекта. rebase HEAD~2
    Очистка проекта

--- Kaihatsu.FileManager/Kaihatsu.FileManager.ConsoleApp/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Kaihatsu.FileManager.ConsoleApp;
-using Kaihatsu.FileManager.Core.Abstraction;
-using Kaihatsu.FileManager.Business;
-
-Console.WriteLine("Hello, World!");
-var item = new Operations();
